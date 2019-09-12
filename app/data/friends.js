69dc786878510967fe8eadd3b422050472d6aff6
BrianKohnen <briankohnen@yahoo.com>
2019-09-12 16:45:01 -0500

    bk--added one dummy data to friends.js for formatting reference. building home page skeleton

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+let frenz = [
+    {
+        name: "Brian",
+        photo: "https://i.pinimg.com/originals/e9/22/95/e9229544228eec1039955067d5a1d8d2.png",
+        scores: [
+            5,
+            2,
+            3,
+            4,
+            1,
+            5,
+            4,
+            4,
+            5,
+            2
+        ]
+    }
+];
