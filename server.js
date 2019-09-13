f104747b21f505f3a37f22e9043d3cab175ffc97
BrianKohnen <briankohnen@yahoo.com>
2019-09-12 20:15:54 -0500

    bk--minor change on line 6 of server.js needed to capitalize PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 let app = express();
 
-let PORT = process.env.port || 8080;
+let PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
