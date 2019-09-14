6740388d1590af169fc84ea00dc13a7913321d4c
BrianKohnen <briankohnen@yahoo.com>
2019-09-14 12:54:20 -0500

    bk--added styling to survey.html inside <head>, added logic to compare friend matches

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,38 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        friendData.push(req.body);
-        res.json(req.body);
+
+        let newFriend = 
+        {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: req.body.scores.map(elem=>parseInt(elem))
+        };
+        
+        let bestScore = 100;
+        let bestie;
+
+        for (var j = 0; j < friendData.length; j++) {
+
+            let currScore = 0;
+            let currArr = [];
+            
+            for (var i = 0; i < newFriend.scores.length; i++) {
+
+                currArr.push(Math.abs(friendData[j].scores[i] - newFriend.scores[i]));
+
+                currScore = currArr.reduce((a,b)=>a+b);
+
+                if (currScore < bestScore) {
+                    bestScore = currScore;
+                    bestie = friendData[j];
+                }
+            }
+        }
+
+        res.json(bestie);
+        friendData.push(newFriend);
+    
     });
 
 }
